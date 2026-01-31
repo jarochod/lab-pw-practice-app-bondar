03aab177e6de9eb8f9b52af1df776746e016bac0
Jaroslaw Chodubski <jaro.chod@gmail.com.com>
2026-01-31 21:00:19 +0100

    s3-ch21 | 21. Tests Structure

---
+++ tests/firstTest.spec.ts
@@ -0,0 +1,7 @@
+import { test } from "@playwright/test";
+
+test("the third test", async ({ page }) => {
+  await page.goto("http://localhost:4200/");
+  await page.getByText("Forms").click();
+  await page.getByText("Form Layouts").click();
+});
