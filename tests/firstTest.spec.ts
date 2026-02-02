6a94650dffd1662817b052b1a20507a19f4ecc85
Jaroslaw Chodubski <jaro.chod@gmail.com.com>
2026-02-03 00:10:05 +0100

    s3-ch22 | 22. Hooks and Flow Control

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,7 +1,29 @@
 import { test } from "@playwright/test";
 
-test("the third test", async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:4200/");
-  await page.getByText("Forms").click();
-  await page.getByText("Form Layouts").click();
+});
+
+test.describe("suite-1", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.getByText("Charts", { exact: true }).click();
+  });
+
+  test("suite-1 | navigate to Echarts page", async ({ page }) => {
+    await page.getByText("Echarts").click();
+  });
+});
+
+test.describe("suite-2", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.getByText("Forms").click();
+  });
+
+  test("suite-2 | the first test", async ({ page }) => {
+    await page.getByText("Form Layouts").click();
+  });
+
+  test("suite-2 | navigate to datepicker page", async ({ page }) => {
+    await page.getByText("Datepicker").click();
+  });
 });
