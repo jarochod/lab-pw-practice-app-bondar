cb9093aa2197d4a764ae79020680eb20ac9e687c
Jaroslaw Chodubski <jaro.chod@gmail.com.com>
2026-04-26 16:12:18 +0200

    s6-ch48 | 48. Locators in Page Objects

--- page-objects/NavigationPage.ts
+++ page-objects/NavigationPage.ts
@@ -1,40 +1,52 @@
-import { Page } from "@playwright/test";
+import { Locator, Page } from "@playwright/test";
 
 // s6-ch46 | 46. First Page Object
 // s6-ch47 | 47. Navigation Page Object
+// s6-ch48 | 48. Locators in Page Objects
 
 // Page Object class to handle main navigation links
 export class NavigationPage {
   readonly page: Page;
+  readonly formLayoutsMenuItem: Locator;
+  readonly datepickerMenuItem: Locator;
+  readonly smartTableMenuItem: Locator;
+  readonly toastrMenuItem: Locator;
+  readonly tooltipMenuItem: Locator;
+
 
   constructor(page: Page) {
     this.page = page;
+    this.formLayoutsMenuItem = page.getByText("Form Layouts");
+    this.datepickerMenuItem = page.getByText("Datepicker");
+    this.smartTableMenuItem = page.getByText("Smart Table");
+    this.toastrMenuItem = page.getByText("Toastr");
+    this.tooltipMenuItem = page.getByText("Tooltip");
   }
 
   // Method to navigate to the Form Layouts section
   async formLayoutsPage() {
     await this.selectGroupMenuItem("Forms");
-    await this.page.getByText("Form Layouts").click();
+    await this.formLayoutsMenuItem.click();
   }
 
   async datepickerPage() {
     await this.selectGroupMenuItem("Forms");
-    await this.page.getByText("Datepicker").click();
+    await this.datepickerMenuItem.click();
   }
 
   async smartTablePage() {
     await this.selectGroupMenuItem("Tables & Data");
-    await this.page.getByText("Smart Table").click();
+    await this.smartTableMenuItem.click();
   }
 
   async toastrPage() {
     await this.selectGroupMenuItem("Modal & Overlays");
-    await this.page.getByText("Toastr").click();
+    await this.toastrMenuItem.click();
   }
 
   async tooltipPage() {
     await this.selectGroupMenuItem("Modal & Overlays");
-    await this.page.getByText("Tooltip").click();
+    await this.tooltipMenuItem.click();
   }
 
   // Private helper to handle menu expansion logic
