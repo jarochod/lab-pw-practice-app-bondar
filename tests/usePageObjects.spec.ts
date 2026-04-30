f3758455b9029532468f02e3cc61737853b58bae
Jaroslaw Chodubski <jaro.chod@gmail.com.com>
2026-04-30 14:25:48 +0200

    s6-ch49 | 49. Parametrized Methods

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,8 +1,10 @@
 import { test } from "@playwright/test";
 import { NavigationPage } from "../page-objects/NavigationPage";
+import { FormLayoutsPage } from "../page-objects/FormLayoutsPage";
 
 // s6-ch46 | 46. First Page Object
 // s6-ch47 | 47. Navigation Page Object
+
 test.beforeEach(async ({ page }) => {
   // Navigate to the local environment before each test execution
   await page.goto("http://localhost:4200/");
@@ -17,4 +19,18 @@ test("navigate to form page", async ({ page }) => {
   await navigateTo.smartTablePage();
   await navigateTo.toastrPage();
   await navigateTo.tooltipPage();
+});
+
+test("parametrized methods test", async ({ page }) => {
+  const navigateTo = new NavigationPage(page);
+  const onFormLayoutsPage = new FormLayoutsPage(page);
+
+  await navigateTo.formLayoutsPage();
+
+  // High-level abstraction: we focus on "what" we do, not "how"
+  await onFormLayoutsPage.submitUsingTheGridFormWithCredentialsAndSelectOptions("test@test.com", "welcome1", "Option 2");
+
+  // Reusing the same logic for different test cases
+  await onFormLayoutsPage.submitInlineFormWithNameEmailAndCheckbox("John Smith", "John@test.com", true);
+  await onFormLayoutsPage.submitInlineFormWithNameEmailAndCheckbox("Jan Kowalski", "test@test.com", false);
 });
