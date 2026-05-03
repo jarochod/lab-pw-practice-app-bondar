5e30d289f77d33c9ed99bd839d063651c840954a
Jaroslaw Chodubski <jaro.chod@gmail.com.com>
2026-05-03 11:47:30 +0200

    s6-ch50 | 50. Date Picker Page Object

---
+++ page-objects/DatepickerPage.ts
@@ -0,0 +1,56 @@
+import { Page, expect } from "@playwright/test";
+
+// s6-ch50 | 50. Date Picker Page Object
+
+export class DatepickerPage {
+  private readonly page: Page;
+
+  constructor(page: Page) {
+    this.page = page;
+  }
+
+  async selectCommonDatePickerDateFromToday(numberOfDaysFromToday: number) {
+    const calendarInputField = this.page.getByPlaceholder("Form Picker");
+    await calendarInputField.click();
+    const dateToAssert = await this.selectDateInTheCalendar(numberOfDaysFromToday);
+    await expect(calendarInputField).toHaveValue(dateToAssert);
+  }
+
+  async selectDatepickerWithRangeFromToday(startDayFromToday: number, endDayFromToday: number) {
+    const calendarInputField = this.page.getByPlaceholder("Range Picker");
+    await calendarInputField.click();
+    const DateToAssertStart = await this.selectDateInTheCalendar(startDayFromToday);
+    const DateToAssertEnd = await this.selectDateInTheCalendar(endDayFromToday);
+    const dateToAssert = `${DateToAssertStart} - ${DateToAssertEnd}`;
+    await expect(calendarInputField).toHaveValue(dateToAssert);
+  }
+
+  /**
+   * Calculates the target date, navigates the calendar to the correct month,
+   * and selects the day.
+   * @param numberOfDaysFromToday - The number of days to add to the current date.
+   * @returns The formatted date string used for assertions.
+   */
+  private async selectDateInTheCalendar(numberOfDaysFromToday: number) {
+    let date = new Date();
+    date.setDate(date.getDate() + numberOfDaysFromToday);
+
+    const expectedDate = date.getDate().toString();
+    const expectedMonthShort = date.toLocaleString("En-US", { month: "short" });
+    const expectedMonthLong = date.toLocaleString("En-US", { month: "long" });
+    const expectedYear = date.getFullYear();
+    const dateToAssert = `${expectedMonthShort} ${expectedDate}, ${expectedYear}`;
+
+    let calendarMonthAndYear = await this.page.locator("nb-calendar-view-mode").textContent();
+    const expectedMonthAndYear = ` ${expectedMonthLong} ${expectedYear} `;
+
+    while (!calendarMonthAndYear.includes(expectedMonthAndYear)) {
+      await this.page.locator('nb-calendar-pageable-navigation [data-name="chevron-right"]').click();
+      calendarMonthAndYear = await this.page.locator("nb-calendar-view-mode").textContent();
+    }
+
+    // Select day while ignoring adjacent month days (.bounding-month)
+    await this.page.locator(".day-cell:not(.bounding-month)").getByText(expectedDate, { exact: true }).click();
+    return dateToAssert;
+  }
+}
--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,9 +1,11 @@
 import { test } from "@playwright/test";
 import { NavigationPage } from "../page-objects/NavigationPage";
 import { FormLayoutsPage } from "../page-objects/FormLayoutsPage";
+import { DatepickerPage } from "../page-objects/DatepickerPage";
 
 // s6-ch46 | 46. First Page Object
 // s6-ch47 | 47. Navigation Page Object
+// s6-ch50 | 50. Date Picker Page Object
 
 test.beforeEach(async ({ page }) => {
   // Navigate to the local environment before each test execution
@@ -24,6 +26,7 @@ test("navigate to form page", async ({ page }) => {
 test("parametrized methods test", async ({ page }) => {
   const navigateTo = new NavigationPage(page);
   const onFormLayoutsPage = new FormLayoutsPage(page);
+  const onDatepickerPage = new DatepickerPage(page);
 
   await navigateTo.formLayoutsPage();
 
@@ -33,4 +36,9 @@ test("parametrized methods test", async ({ page }) => {
   // Reusing the same logic for different test cases
   await onFormLayoutsPage.submitInlineFormWithNameEmailAndCheckbox("John Smith", "John@test.com", true);
   await onFormLayoutsPage.submitInlineFormWithNameEmailAndCheckbox("Jan Kowalski", "test@test.com", false);
+
+  // Reusing methods for different date picker components
+  await navigateTo.datepickerPage();
+  await onDatepickerPage.selectCommonDatePickerDateFromToday(5);
+  await onDatepickerPage.selectDatepickerWithRangeFromToday(5,8);
 });
